adb5d8177f5961af358360a51b76884a23cc54dc
David Scott <dave.scott@eu.citrix.com>
2013-02-04 11:44:23 +0000

    Respond to a query message, return version information
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- server.ml
+++ server.ml
@@ -12,12 +12,45 @@
  * GNU Lesser General Public License for more details.
  *)
 
+let driver = "ffs"
 let name = "ffs"
-let major_version = 0
+let description = "Flat File Storage Repository for XCP"
+let vendor = "Citrix"
+let copyright = "Citrix Inc"
 let minor_version = 1
+let major_version = 0
+let version = Printf.sprintf "%d.%d" major_version minor_version
+let required_api_version = "2.0"
+let features = []
+let configuration = []
 
 module Implementation = struct
-  include Storage_skeleton
+  type context = unit
+  open Storage_interface
+
+  module Query = struct
+    let query ctx ~dbg = {
+        driver;
+        name;
+        description;
+        vendor;
+        copyright;
+        version;
+        required_api_version;
+        features;
+        configuration;
+    }
+
+    let diagnostics ctx ~dbg = "Not available"
+  end
+  module DP = struct include Storage_skeleton.DP end
+  module VDI = struct include Storage_skeleton.VDI end
+  module SR = struct include Storage_skeleton.SR end
+  module UPDATES = struct include Storage_skeleton.UPDATES end
+  module TASK = struct include Storage_skeleton.TASK end
+  module Policy = struct include Storage_skeleton.Policy end
+  module DATA = struct include Storage_skeleton.DATA end
+  let get_by_name = Storage_skeleton.get_by_name
 end
 
 module Server = Storage_interface.Server(Implementation)
