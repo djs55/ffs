e8f7493ccce4ce8aaf1e72372e44c697ed871358
David Scott <dave.scott@eu.citrix.com>
2013-06-29 12:36:39 +0000

    Move utility functions to Common; add 'type format = Vhd | Raw'
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- common.ml
+++ common.ml
@@ -12,6 +12,11 @@
  * GNU Lesser General Public License for more details.
  *)
 
+open Xcp_service
+
+module D = Debug.Make(struct let name = "ffs" end)
+include D
+
 let finally f g =
   try
     let result = f () in
@@ -21,8 +26,78 @@ let finally f g =
     g ();
     raise e
 
-open Xcp_service
+let string_of_file filename =
+  let ic = open_in filename in
+  let output = Buffer.create 1024 in
+  try
+    while true do
+      let block = String.make 4096 '\000' in
+      let n = input ic block 0 (String.length block) in
+      if n = 0 then raise End_of_file;
+      Buffer.add_substring output block 0 n
+    done;
+    "" (* never happens *)
+  with End_of_file ->
+    close_in ic;
+    Buffer.contents output
 
-module D = Debug.Make(struct let name = "ffs" end)
-include D
+let file_of_string filename string =
+  let oc = open_out filename in
+  finally
+    (fun () ->
+      debug "write >%s %s" filename string;
+      output oc string 0 (String.length string)
+    ) (fun () -> close_out oc)
+
+let startswith prefix x =
+  let prefix' = String.length prefix in
+  let x' = String.length x in
+  x' >= prefix' && (String.sub x 0 prefix' = prefix)
+
+let remove_prefix prefix x =
+  let prefix' = String.length prefix in
+  let x' = String.length x in
+  String.sub x prefix' (x' - prefix')
+
+let endswith suffix x =
+  let suffix' = String.length suffix in
+  let x' = String.length x in
+  x' >= suffix' && (String.sub x (x' - suffix') suffix' = suffix)
+
+let iso8601_of_float x = 
+  let time = Unix.gmtime x in
+  Printf.sprintf "%04d%02d%02dT%02d:%02d:%02dZ"
+    (time.Unix.tm_year+1900)
+    (time.Unix.tm_mon+1)
+    time.Unix.tm_mday
+    time.Unix.tm_hour
+    time.Unix.tm_min
+    time.Unix.tm_sec
+
+
+(** create a directory, and create parent if doesn't exist *)
+let mkdir_rec dir perm =
+  let mkdir_safe dir perm =
+    try Unix.mkdir dir perm with Unix.Unix_error (Unix.EEXIST, _, _) -> () in
+  let rec p_mkdir dir =
+    let p_name = Filename.dirname dir in
+    if p_name <> "/" && p_name <> "." 
+    then p_mkdir p_name;
+    mkdir_safe dir perm in
+  p_mkdir dir
+
+let rm_f x = try Unix.unlink x with _ -> ()
+
+let ( |> ) a b = b a
+
+let retry_every n f =
+  let finished = ref false in
+  while (not !finished) do
+    try
+      let () = f () in
+      finished := true;
+    with e ->
+      debug "Caught %s: sleeping %f. before trying again" (Printexc.to_string e) n;
+      Thread.delay n
+  done
 
--- server.ml
+++ server.ml
@@ -47,82 +47,21 @@ type sr = {
 } with rpc
 type srs = (string * sr) list with rpc
 
-let string_of_file filename =
-  let ic = open_in filename in
-  let output = Buffer.create 1024 in
-  try
-    while true do
-      let block = String.make 4096 '\000' in
-      let n = input ic block 0 (String.length block) in
-      if n = 0 then raise End_of_file;
-      Buffer.add_substring output block 0 n
-    done;
-    "" (* never happens *)
-  with End_of_file ->
-    close_in ic;
-    Buffer.contents output
-
-let file_of_string filename string =
-  let oc = open_out filename in
-  finally
-    (fun () ->
-      debug "write >%s %s" filename string;
-      output oc string 0 (String.length string)
-    ) (fun () -> close_out oc)
-
-let startswith prefix x =
-  let prefix' = String.length prefix in
-  let x' = String.length x in
-  x' >= prefix' && (String.sub x 0 prefix' = prefix)
-
-let remove_prefix prefix x =
-  let prefix' = String.length prefix in
-  let x' = String.length x in
-  String.sub x prefix' (x' - prefix')
-
-let endswith suffix x =
-  let suffix' = String.length suffix in
-  let x' = String.length x in
-  x' >= suffix' && (String.sub x (x' - suffix') suffix' = suffix)
-
-let iso8601_of_float x = 
-  let time = Unix.gmtime x in
-  Printf.sprintf "%04d%02d%02dT%02d:%02d:%02dZ"
-    (time.Unix.tm_year+1900)
-    (time.Unix.tm_mon+1)
-    time.Unix.tm_mday
-    time.Unix.tm_hour
-    time.Unix.tm_min
-    time.Unix.tm_sec
-
-
-(** create a directory, and create parent if doesn't exist *)
-let mkdir_rec dir perm =
-  let mkdir_safe dir perm =
-    try Unix.mkdir dir perm with Unix.Unix_error (Unix.EEXIST, _, _) -> () in
-  let rec p_mkdir dir =
-    let p_name = Filename.dirname dir in
-    if p_name <> "/" && p_name <> "." 
-    then p_mkdir p_name;
-    mkdir_safe dir perm in
-  p_mkdir dir
-
-let rm_f x = try Unix.unlink x with _ -> ()
-
-let ( |> ) a b = b a
-
-let retry_every n f =
-  let finished = ref false in
-  while (not !finished) do
-    try
-      let () = f () in
-      finished := true;
-    with e ->
-      debug "Caught %s: sleeping %f. before trying again" (Printexc.to_string e) n;
-      Thread.delay n
-  done
-
 open Storage_interface
+
+type format =
+  | Vhd
+  | Raw
+
+let format_of_vdi_info x =
+  if List.mem_assoc "type" x.sm_config
+  then match String.lowercase (List.assoc "type" x.sm_config) with
+    | "vhd" -> Some Vhd
+    | "raw" -> Some Raw
+    | y ->
+      warn "Unknown sm-config:type=%s for vdi %s/%s" y x.name_label x.vdi;
+      None
+  else None
 
 module Attached_srs = struct
   let table = Hashtbl.create 16
