c6129139ca0793af2bf847103380486b8beb0c7f
David Scott <dave.scott@eu.citrix.com>
2014-01-16 15:58:05 +0000

    travis: install blktap-dev, for libvhd.h, for vhdlib
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,6 +8,7 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install blktap-dev # for libvhd.h
 export OPAMYES=1
 export OPAMVERBOSE=1
 
