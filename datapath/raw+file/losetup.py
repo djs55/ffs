3275d5529b8c6148b2de2d409d4571dbb884bc31
David Scott <dave.scott@citrix.com>
2015-05-06 20:32:40 +0100

    losetup: use a module rather than a singleton object
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- datapath/raw+file/losetup.py
+++ datapath/raw+file/losetup.py
@@ -16,24 +16,24 @@ def run(dbg, cmd):
     return output
 
 # Use Linux "losetup" to create block devices from files
-class Losetup:
-    # [_find dbg path] returns the loop device associated with [path]
-    def _find(self, dbg, path):
-        for line in run(dbg, "losetup -a").split("\n"):
-            line = line.strip()
-            if line <> "":
-                bits = line.split()
-                loop = bits[0][0:-1]
-                this_path = bits[2][1:-1]
-                if this_path == path:
-                    return loop
-        return None
-    # [add dbg path] creates a new loop device for [path] and returns it
-    def add(self, dbg, path):
-        run(dbg, "losetup -f %s" % path)
-        return self._find(dbg, path)
-    # [remove dbg path] removes the loop device associated with [path]
-    def remove(self, dbg, path):
-        loop = self._find(dbg, path)
-        run(dbg, "losetup -d %s" % loop)
+
+# [_find dbg path] returns the loop device associated with [path]
+def _find(dbg, path):
+    for line in run(dbg, "losetup -a").split("\n"):
+        line = line.strip()
+        if line <> "":
+            bits = line.split()
+            loop = bits[0][0:-1]
+            this_path = bits[2][1:-1]
+            if this_path == path:
+                return loop
+    return None
+# [add dbg path] creates a new loop device for [path] and returns it
+def add(dbg, path):
+    run(dbg, "losetup -f %s" % path)
+    return _find(dbg, path)
+# [remove dbg path] removes the loop device associated with [path]
+def remove(dbg, path):
+    loop = _find(dbg, path)
+    run(dbg, "losetup -d %s" % loop)
 
