601c790de10787a2d158dd380416762d9b3cca7c
David Scott <dave@recoil.org>
2015-07-01 15:59:27 +0100

    Stand-alone tapdisk module experiment
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/tapdisk.py
+++ lib/tapdisk.py
@@ -1,23 +1,58 @@
 #!/usr/bin/env python
 
-import os
-import signal
-import socket
+import os, sys, signal, socket, subprocess
 
-# from python-fdsend
-import fdsend
+"""
+import tapdisk
 
-import xapi
-import commands
-import image
-from common import log, call
+# to create a block device
+t = tapdisk.create()
+t.open(tapdisk.Raw("foo.img"))
+print t.block_device()
+
+# to shut down
+t.destroy()
+"""
 
 # Use Xen tapdisk to create block devices from files
 
 blktap2_prefix = "/dev/xen/blktap-2/tapdev"
 
-nbdclient_prefix = "/var/run/blktap-control/nbdclient"
-nbdserver_prefix = "/var/run/blktap-control/nbdserver"
+def log(txt):
+    print >>sys.stderr, txt
+
+# [call cmd_args] executes [cmd_args], throwing a BackendError if exits with
+# a non-zero exit code.
+def call(cmd_args):
+    p = subprocess.Popen(cmd_args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = p.communicate ()
+    if p.returncode <> 0:
+        log("%s exitted with code %d: %s" % (" ".join(cmd_args), p.returncode, stderr))
+        raise (Exception("%s exitted with non-zero code %d: %s" % (" ".join(cmd_args), p.returncode, stderr)))
+    return stdout
+
+class Path:
+    """An entity on the filesystem"""
+    def __init__(self, path):
+        self.path = path
+
+class Vhd(Path):
+    """An entity on the filesystem in vhd format"""
+    def __init__(self, path):
+        Path.__init__(self, path)
+    def format(self):
+        return "vhd"
+    def __str__(self):
+        return "vhd:" + self.path
+
+class Raw(Path):
+    """An entity on the filesystem in raw format"""
+    def __init__(self, path):
+        Path.__init__(self, path)
+    def format(self):
+        return "raw"
+    def __str__(self):
+        return "aio:" + self.path
 
 class Tapdisk:
     def __init__(self, minor, pid, f):
@@ -25,49 +60,30 @@ class Tapdisk:
         self.pid = pid
         self.f = f
         self.secondary = None # mirror destination
-    def destroy(self, dbg):
-        self.pause(dbg)
-        call(dbg, ["tap-ctl", "destroy", "-m", str(self.minor), "-p", str(self.pid) ])
-    def close(self, dbg):
-        call(dbg, ["tap-ctl", "close", "-m", str(self.minor), "-p", str(self.pid) ])
+    def destroy(self):
+        self.pause()
+        call(["tap-ctl", "destroy", "-m", str(self.minor), "-p", str(self.pid) ])
+    def close(self ):
+        call(["tap-ctl", "close", "-m", str(self.minor), "-p", str(self.pid) ])
         self.f = None
-    def open(self, dbg, f):
-        assert (isinstance(f, image.Vhd) or isinstance(f, image.Raw))
-        call(dbg, ["tap-ctl", "open", "-m", str(self.minor), "-p", str(self.pid), "-a", str(f)])
+    def open(self, f):
+        assert (isinstance(f, Vhd) or isinstance(f, Raw))
+        call(["tap-ctl", "open", "-m", str(self.minor), "-p", str(self.pid), "-a", str(f)])
         self.f = f
-    def pause(self, dbg):
-        call(dbg, ["tap-ctl", "pause", "-m", str(self.minor), "-p", str(self.pid)])
-    def unpause(self, dbg):
+    def pause(self):
+        call(["tap-ctl", "pause", "-m", str(self.minor), "-p", str(self.pid)])
+    def unpause(self):
         cmd = ["tap-ctl", "unpause", "-m", str(self.minor), "-p", str(self.pid) ]
         if self.secondary is not None:
             cmd = cmd + [ "-2 ", self.secondary ]
-        call(dbg, cmd)
+        call(cmd)
     def block_device(self):
         return blktap2_prefix + str(self.minor)
-    def start_mirror(self, dbg, fd):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect(nbdclient_prefix + str(self.pid))
-        token = "token"
-        fdsend.sendfds(sock, token, fds = [ fd ])
-        sock.close()
-        self.secondary = "nbd:" + token
-        self.pause(dbg)
-        self.unpause(dbg)
-    def stop_mirror(self, dbg):
-        self.secondary = None
-        self.pause(dbg)
-        self.unpause(dbg)
-    def receive_nbd(self, dbg, fd):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect("%s%d.%d" % (nbdserver_prefix, self.pid, self.minor))
-        token = "token"
-        fdsend.sendfds(sock, token, fds = [ fd ])
-        sock.close()
 
-def create(dbg):
-    output = call(dbg, ["tap-ctl", "spawn"]).strip()
+def create():
+    output = call(["tap-ctl", "spawn"]).strip()
     pid = int(output)
-    output = call(dbg, ["tap-ctl", "allocate"]).strip()
+    output = call(["tap-ctl", "allocate"]).strip()
     prefix = blktap2_prefix
     minor = None
     if output.startswith(prefix):
@@ -75,12 +91,12 @@ def create(dbg):
     if minor is None:
         os.kill(pid, signal.SIGQUIT)
         raise (xapi.InternalError("tap-ctl allocate returned unexpected output: '%s'" % output))
-    call(dbg, ["tap-ctl", "attach", "-m", str(minor), "-p", str(pid) ])
+    call( ["tap-ctl", "attach", "-m", str(minor), "-p", str(pid) ])
     return Tapdisk(minor, pid, None)
 
-def list(dbg):
+def list():
     results = []
-    for line in call(dbg, ["tap-ctl", "list"]).split("\n"):
+    for line in call(["tap-ctl", "list"]).split("\n"):
         bits = line.split()
         if bits == []:
             continue
@@ -102,16 +118,16 @@ def list(dbg):
                 this = None
                 prefix = "aio:"
                 if args.startswith(prefix):
-                    this = image.Raw(args[len(prefix):])
+                    this = Raw(args[len(prefix):])
                     results.append(Tapdisk(minor, pid, this))
                 prefix = "vhd:"
                 if args.startswith(prefix):
-                    this = image.Vhd(args[len(prefix):])
+                    this = Vhd(args[len(prefix):])
                     results.append(Tapdisk(minor, pid, this))
     return results
 
-def find_by_file(dbg, f):
-    assert (isinstance(f, image.Vhd) or isinstance(f, image.Raw))
-    for tapdisk in list(dbg):
+def find_by_file(f):
+    assert (isinstance(f, Vhd) or isinstance(f, Raw))
+    for tapdisk in list():
         if str(f) == str(tapdisk.f):
             return tapdisk
