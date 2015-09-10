a7b3957df16ec39b9322e1086d1a0ac15bb8869e
David Scott <dave.scott@eu.citrix.com>
2015-09-10 17:14:23 +0100

    Remove 'call': it was moved to xapi.storage.common
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/ffs/common.py
+++ lib/ffs/common.py
@@ -7,29 +7,6 @@ import xapi
 import subprocess
 
 
-# [call dbg cmd_args] executes [cmd_args]
-# if [error] and exit code != expRc, log and throws a BackendError
-# if [simple], returns only stdout
-
-
-def call(dbg, cmd_args, error=True, simple=True, expRc=0):
-    log.debug("%s: Running cmd %s" % (dbg, cmd_args))
-    p = subprocess.Popen(
-        cmd_args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        close_fds=True)
-    stdout, stderr = p.communicate()
-    if error and p.returncode != expRc:
-        log.error("%s: %s exitted with code %d: %s" %
-                  (dbg, " ".join(cmd_args), p.returncode, stderr))
-        raise xapi.InternalError("%s exitted with non-zero code %d: %s"
-                                 % (" ".join(cmd_args), p.returncode, stderr))
-    if simple:
-        return stdout
-    return stdout, stderr, p.returncode
-
-
 # Attempts to create an empty regular file with the given prefix and suffix
 # If the name "prefix.suffix" exists, this will try "prefix.%d.suffix" until
 # a unique name has been found. Suffix may be an empty string.
